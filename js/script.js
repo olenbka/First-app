acad6e48b576785eb81441479f8917be38453079
Olga <nadolga.nl@gmail.com>
2021-08-30 11:27:09 +0200

    modified js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /*Задание 1*/
 const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
 /*Задание 2*/
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -8,7 +9,7 @@ const personalMovieDB = {
     gerners: [],
     privat: false
 };
-/*Задание 3*/
+/*Задание 3
 const a = prompt('Один из последних просмотренных фильмов?', ''),
     b = prompt('Какую оценку вы ему поставите?', ''),
     c = prompt('Один из последних просмотренных фильмов?', ''),
@@ -17,4 +18,26 @@ const a = prompt('Один из последних просмотренных ф
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
+console.log(personalMovieDB);
+*/
+
+
+
+
+/*Задание 4*/
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('Какую оценку вы ему поставите?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('jjj');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+
+}
 console.log(personalMovieDB);
